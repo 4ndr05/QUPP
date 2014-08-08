1694a301d74546b76e22a9f9bf780201b42b4a58
fraleiz17 <fraleiz17@gmail.com>
2014-08-08 00:11:06 -0500

    Modelo Perfil
    Init

---
+++ application/models/perfil_model.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined('BASEPATH'))
+    die("No hay acceso directo a este script");
+
+/*
+ * Modelo para manejar datos defaults
+ */
+
+class Perfil_model extends CI_Model{
+
+
+}
+
+?>
